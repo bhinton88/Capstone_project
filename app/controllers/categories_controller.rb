713e9603bfeb82f5cfa1cbe67b41c1fdff495ed4
Bobby Hinton <bob.g.hint@gmail.com>
2023-05-29 22:15:38 -0500

    added validations to customer model. installed stripe gem

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,4 @@
+class CategoriesController < ApplicationController
+
+  # create logic that will take in a category name and return the items from that category,will happen on click of a box with respective category name in it 
+end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,17 @@
 class Customer < ApplicationRecord
+  has_secure_password
+  has_many :orders
+
+  validates :username, :address, :city, :state, :email, :stripe_id, presence: true
+
+  before_validation :create_on_stripe, on: :create
+
+  #  for the email and name, since this is an INSTANCE method, the self is implicit
+
+  def create_on_stripe
+    params = { email: email, name: name }  
+    response = Stripe::Customer.create(params)
+    self.stripe_id = response.id
+  end
+
 end
